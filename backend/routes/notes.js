e956c30a42c4f9402784f23219eceb32bc281b3c
ManishBisht <manishbisht9711@gmail.com>
2021-09-09 20:23:04 +0530

    adding and fetching notes of user

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,45 @@
 const express = require("express");
-
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/notes");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//route :1 fetching all notes of user :-> login required
+
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json([notes]);
 });
+
+//route :2 adding new notes :-> login required :/api/notes/addnote
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Please Enter a valid title").isLength({ min: 3 }),
+    body("description", "enter a valid description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const error = validationResult(req);
+      if (!error.isEmpty()) {
+        return res.status(400).json({ error: error.array() });
+      }
+
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json({ savedNote });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+);
 
 module.exports = router;
