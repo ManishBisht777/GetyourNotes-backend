19709f6ce6c7967a9c46a1f33940629596e9f494
ManishBisht <manishbisht9711@gmail.com>
2021-09-12 18:56:36 +0530

    fetching notes in context api

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,10 @@
-import React, { useContext, useEffect } from "react";
-import noteContext from "../context/notes/notesContext";
+import React from "react";
+// import noteContext from "../context/notes/notesContext";
 
 function About() {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-  return (
-    <div>
-      This is about {a.state.name} of age {a.state.age}
-    </div>
-  );
+  // const a = useContext(noteContext);
+
+  return <div>This is about page</div>;
 }
 
 export default About;
